08e37635e99e56c84a0daa3f7f595a6d7f19042d
ArulGnani <version47@gmail.com>
2020-09-05 14:37:07 +0530

    added infinity scrolling to meme-page

--- src/components/meme-page/meme.tsx
+++ src/components/meme-page/meme.tsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 interface Iprop {
-    url: string,
-    mid: string
+    url: string;
+    mid: string;
 }
 
 export const Meme: React.FC<Iprop> = ({ url, mid }) => {
+    const [memeUrl, setMemeUrl] = useState("")
+    const [memeId, setMemeId] = useState("")
+
+    useEffect(() => {
+        setMemeUrl(url)
+        setMemeId(mid)
+    }, [])
+
+    const download = async () => {
+        console.log(memeUrl, memeId)
+
+        // download meme
+
+        await updateDownloadCount()
+    }
+
+
     return (
-        <img 
-            src={url} 
-            alt="meme"
-            key={mid}
-            width="300px"
-            height="200px"
-        />
+        <React.Fragment> 
+            <img 
+                src={url} alt="meme" key={mid}
+                width="300px" height="200px"
+            />
+            <section>
+                <button onClick={download}> 
+                    download 
+                </button>    
+            </section>
+        </React.Fragment>
     )
+}
+
+const updateDownloadCount = async () => {
+    // update-download-count 
 }
--- src/components/meme-page/memes-comp.tsx
+++ src/components/meme-page/memes-comp.tsx
@@ -1,11 +1,61 @@
-import React, { useEffect, useState, useReducer } from 'react'
+import React, { 
+    useEffect, useState, useReducer, 
+    useRef, useCallback 
+} from 'react'
 import './style/meme-comp.css'
 import { reducer, initState, ImemePage } from '../welcome-screen/context'
 import { Meme } from './meme'
 
+
 export const MemeComp: React.FC = () => {
     const [state, dispatch] = useReducer(reducer, initState)
     const [memes, setMemes] = useState<ImemePage[]>([])
+    const [loading, setLoading] = useState(false)
+    const end = useRef(false)
+
+    const observer = useRef<IntersectionObserver>()
+    const lastRef = useCallback(node => {
+
+        if (observer.current) observer.current.disconnect()
+        
+        observer.current = new IntersectionObserver(e => {
+
+            if (e[0].isIntersecting && end.current === false) {
+                console.log('get memes', end)
+                getMemes()
+            }
+
+        },{ threshold: 1 })
+
+        if (node) observer.current.observe(node)
+
+    }, [])
+
+    const getMemes = async () => {
+        setLoading(true)
+
+        let lid = sessionStorage.getItem("lid") ? sessionStorage.getItem("lid") : null
+        let nxt = lid === null ? memes.length + 1 : parseInt(lid)
+
+        console.log(nxt)
+        await fetch(`http://localhost:5000/api/get/memes/${nxt}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setLoading(false)
+
+                if (data.length === 0) {
+                    end.current = true
+                    alert("ur all caught up, try again later for new memes")
+                }
+
+                setMemes(existingMemes => [...existingMemes, ...data])
+
+                sessionStorage.setItem("lid", nxt + data.length)
+            })
+            .catch((_) => alert("error while fetching data."))
+    }
+
 
     useEffect(() => {
         if (window.performance.navigation.type === 1 || 
@@ -13,7 +63,7 @@ export const MemeComp: React.FC = () => {
             
             let lastReqTime = sessionStorage.getItem("lastReqTime")
             
-            let nextReqTime = lastReqTime ? parseInt(lastReqTime) + 5 * 60000 : 0
+            let nextReqTime = lastReqTime ? parseInt(lastReqTime) + 10 * 60000 : 0
 
             if (nextReqTime === null) getLatestMemes()
 
@@ -41,7 +91,8 @@ export const MemeComp: React.FC = () => {
                 setMemes(existingMemes => [...existingMemes, ...data])
                 
                 sessionStorage.setItem("lastReqTime", new Date().getTime().toString())
-                
+                sessionStorage.setItem("lid", data.length)
+
                 sessionStorage.setItem("memes", JSON.stringify([...memes, ...data]))
                 
                 dispatch({ 
@@ -54,21 +105,20 @@ export const MemeComp: React.FC = () => {
             })
     }
 
-    const singleMeme = () => {
-        console.log("meme div")
-    }
 
     return (
         <section id="memes-comp">
-            { memes.map(meme => {
+            { memes.map((meme, idx) => {
                 return (
-                    <div key={meme.mid} onClick={singleMeme}>
+                    <div ref={idx + 1 === memes.length ? lastRef : null} 
+                        key={meme.mid} >
                         <Meme url={meme.url} mid={meme.mid} 
-                            key={meme.mid}
+                            key={meme.mid} 
                         />
                     </div>
-                )
+                ) 
             })}
+            { loading ? <div id="loading"> loading... </div> : null }
         </section>
     )
 }
